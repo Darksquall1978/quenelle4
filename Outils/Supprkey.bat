9d0c002bd348586acfcef8baa5c181abcbba521d
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2022-07-05 19:55:56 +0200

    Add files via upload

---
+++ Outils/Supprkey.bat
@@ -0,0 +1,26 @@
+title Suppr cd-key
+@echo off
+Color 0b & Mode con cols=80 lines=2
+
+::Color 0b & Mode con cols=80 lines=50
+
+
+cd..
+cd..
+cd..
+cd..
+cd..
+
+
+
+
+del "%USERPROFILE%\Documents\My Games\Unreal Tournament 3\UTGame\Config\UTEngine.ini
+
+
+
+
+
+echo Cle cd Supprimer
+
+timeout /t 5 /nobreak > nul
+::pause>nul
